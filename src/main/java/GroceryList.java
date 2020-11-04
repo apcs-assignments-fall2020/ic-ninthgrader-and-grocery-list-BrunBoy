bdc8b36420b04f665786750ad746608d4b3b84bb
BrunBoy <23rreddy@athenian.org>
2020-11-04 04:09:52 +0000

    amIDone?

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -12,17 +12,19 @@ public class GroceryList {
         }
     }
     public void remove(String item){
+        //help from kyra mccarrick. my shifting them to the left didn't work properly. she explained it to me.
         for (int i=0; i<arr.length; i++){
-            if (arr[i]==null){
-                arr[i]="";
-                break;
+            if (arr[i]==item){
+                for(int j=i; j<arr.length-1; j++){
+                    arr[j]=arr[j+1];
+                }
             }
         }
     }
-    public String toString(String item){
+    public String toString(){
         String gList="Grocery List: ";
         for (int i=0; i<arr.length; i++){
-            gList+=arr[i]+"";
+            gList+=arr[i]+" ";
         }
         return gList;
     }
