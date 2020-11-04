c3e6e8fd8beba8ebd7487e65e1e4707abc63e624
BrunBoy <23rreddy@athenian.org>
2020-11-04 01:57:20 +0000

    notDoneYet

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,25 @@
 public class GroceryList {
-    
+    private String[] arr;
+    public GroceryList() {
+        arr=new String[10];
+    }
+    public void add(String item){
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]==null){
+                arr[i]=item;
+                break;
+            }
+        }
+    }
+    public void remove(String item){
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]==null){
+                arr[i]="";
+                break;
+            }
+        }
+    }
+    public String toString(String item){
+        return item;
+    }
 }
